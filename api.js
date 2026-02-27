26f65fcc73c98f23e96fa7615c9d0206e9e9a41a
dechris12 <christianstephen009@gmail.com>
2026-02-27 07:27:11 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,21 +1,14 @@
-/**
- * Cyber Sweeft - API Configuration
- */
-
 const API_CONFIG = {
   // PAYSTACK KEYS
-  PAYSTACK_PUBLIC_KEY: 'pk_live_your_public_key_here', 
-  PAYSTACK_SECRET_KEY: 'sk_live_your_secret_key_here',
+  PAYSTACK_PUBLIC_KEY: 'pk_live_your_actual_key', 
+  PAYSTACK_SECRET_KEY: 'sk_live_your_actual_key',
   
   // GOOGLE SHEETS
-  // IMPORTANT: Sheet MUST be set to "Anyone with the link can view"
-  SHEET_ID: '1mtOy-WiKy1Jd85amuJeh_JLWxWlAVvCr', 
+  // Sheet MUST be set to "Anyone with the link can view"
+  SHEET_ID: '1CdOJ_j-yT7MudoCRd4GFL7bBsKs_wK96ipIStAKO7XY', 
   SHEET_NAME: 'sweeft projects',
   
-  // PRICING
   FIXED_PRICE: 2500,
-  
-  // STORAGE KEY
   STORAGE_KEY: 'cybersweeft_purchases_v1'
 };
 
